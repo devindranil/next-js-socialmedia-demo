52c8d763d59b5bbee631cce41bab4bbac80e17c3
learnshyindranil <ic880572@gmail.com>
2025-01-11 20:18:05 +0530

    part3 and part4

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Lama Dev Social Media App",
+  title: "Social Media App",
   description: "Social media app built with Next.js",
 };
 
